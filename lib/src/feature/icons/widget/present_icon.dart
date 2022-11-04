31bea434c0d1f4f3298fe92ec8cac6e03cb1650d
Plague Fox <plugfox@gmail.com>
2022-11-05 02:56:09 +0400

    Add candies to icon

--- lib/src/feature/icons/widget/present_icon.dart
+++ lib/src/feature/icons/widget/present_icon.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -20,7 +24,7 @@ class PresentIcon extends StatefulWidget {
 }
 
 /// State for widget PresentIcon
-class _PresentIconState extends State<PresentIcon> with SingleTickerProviderStateMixin {
+class _PresentIconState extends State<PresentIcon> with _OverlayCandiesMixin, SingleTickerProviderStateMixin {
   static const Duration _duration = Duration(milliseconds: 6000);
   late AnimationController _controller;
   late Animation<double> _turns, _push, _me, _down, _hide;
@@ -52,7 +56,7 @@ class _PresentIconState extends State<PresentIcon> with SingleTickerProviderStat
         TweenSequenceItem<double>(tween: Tween<double>(begin: 0, end: -.08), weight: 1),
         TweenSequenceItem<double>(tween: Tween<double>(begin: -.08, end: 0), weight: 1.5),
       ],
-    ).animate(CurvedAnimation(parent: _controller, curve: const Interval(0, .5, curve: Curves.easeInOut)));
+    ).animate(CurvedAnimation(parent: _controller, curve: const Interval(.25, .75, curve: Curves.easeInOut)));
     _push = CurvedAnimation(parent: _controller, curve: const Interval(.3, .5, curve: Curves.easeInOut));
     _me = CurvedAnimation(parent: _controller, curve: const Interval(.4, .6, curve: Curves.easeInOut));
     _down = CurvedAnimation(parent: _controller, curve: const Interval(.5, .7, curve: Curves.easeInOut));
@@ -68,14 +72,17 @@ class _PresentIconState extends State<PresentIcon> with SingleTickerProviderStat
   /* #endregion */
 
   Future<void> _onPressed() async {
+    if (overlayStatus.value) return;
     try {
+      show();
       _controller.reset();
-    } on TickerCanceled {
-      // the animation is no longer running
+      HapticFeedback.mediumImpact().ignore();
+      await Future<void>.delayed(const Duration(milliseconds: 2000));
+    } on Object {
+      rethrow;
     } finally {
-      _controller
-        ..reset()
-        ..repeat();
+      hide();
+      _controller.repeat().ignore();
     }
   }
 
@@ -86,29 +93,38 @@ class _PresentIconState extends State<PresentIcon> with SingleTickerProviderStat
             children: <Widget>[
               Positioned.fill(
                 child: Center(
-                  child: RotationTransition(
-                    turns: _turns,
-                    child: IconButton(
-                      onPressed: () {},
-                      iconSize: widget.size,
-                      color: Colors.pink,
-                      padding: EdgeInsets.zero,
-                      constraints: BoxConstraints.expand(width: widget.size, height: widget.size),
-                      splashRadius: widget.size * .75,
-                      icon: const Icon(
-                        FontAwesomeIcons.gift,
-                        shadows: [
-                          BoxShadow(
-                            color: Colors.black,
-                            offset: Offset.zero,
-                            blurRadius: 2,
+                  child: buildOverlay(
+                    child: RotationTransition(
+                      turns: _turns,
+                      child: ValueListenableBuilder<bool>(
+                        valueListenable: overlayStatus,
+                        builder: (context, status, child) => AnimatedScale(
+                          duration: const Duration(milliseconds: 500),
+                          scale: status ? .75 : 1,
+                          child: IconButton(
+                            onPressed: status ? null : _onPressed,
+                            iconSize: widget.size,
+                            color: Colors.pink,
+                            padding: EdgeInsets.zero,
+                            constraints: BoxConstraints.expand(width: widget.size, height: widget.size),
+                            splashRadius: widget.size * .75,
+                            icon: const Icon(
+                              FontAwesomeIcons.gift,
+                              shadows: <Shadow>[
+                                BoxShadow(
+                                  color: Colors.black,
+                                  offset: Offset.zero,
+                                  blurRadius: 2,
+                                ),
+                                BoxShadow(
+                                  color: Colors.black,
+                                  offset: Offset.zero,
+                                  blurRadius: 4,
+                                ),
+                              ],
+                            ),
                           ),
-                          BoxShadow(
-                            color: Colors.black,
-                            offset: Offset.zero,
-                            blurRadius: 4,
-                          ),
-                        ],
+                        ),
                       ),
                     ),
                   ),
@@ -216,4 +232,272 @@ class _PresentIconWord extends StatelessWidget {
           ),
         ),
       );
+}
+
+mixin _OverlayCandiesMixin on State<PresentIcon> {
+  @protected
+  final ValueNotifier<bool> overlayStatus = ValueNotifier<bool>(false);
+  final LayerLink _layerLink = LayerLink();
+  OverlayEntry? _overlayEntry;
+
+  @protected
+  void show() {
+    hide();
+    Overlay.of(context)?.insert(
+      _overlayEntry = OverlayEntry(
+        builder: (context) => Positioned(
+          height: widget.size * 3,
+          width: widget.size * 3,
+          child: CompositedTransformFollower(
+            link: _layerLink,
+            offset: Offset.zero,
+            targetAnchor: Alignment.center,
+            followerAnchor: Alignment.center,
+            showWhenUnlinked: false,
+            child: _CandiesOverlay(onCompleted: hide),
+          ),
+        ),
+      ),
+    );
+    overlayStatus.value = _overlayEntry != null;
+  }
+
+  @protected
+  void hide() {
+    overlayStatus.value = false;
+    if (_overlayEntry == null) return;
+    _overlayEntry?.remove();
+    _overlayEntry = null;
+  }
+
+  @override
+  void dispose() {
+    hide();
+    super.dispose();
+  }
+
+  @protected
+  Widget buildOverlay({required Widget child}) => CompositedTransformTarget(
+        link: _layerLink,
+        child: child,
+      );
+}
+
+class _CandiesOverlay extends StatefulWidget {
+  const _CandiesOverlay({required this.onCompleted});
+
+  final VoidCallback onCompleted;
+
+  @override
+  State<_CandiesOverlay> createState() => _CandiesOverlayState();
+}
+
+class _CandiesOverlayState extends State<_CandiesOverlay> with SingleTickerProviderStateMixin {
+  static const Duration _duration = Duration(milliseconds: 2000);
+  static final math.Random _random = math.Random();
+  static const int minCandies = 16, maxCandies = 24;
+  static List<IconData> get icons => <IconData>[
+        Icons.cake,
+        Icons.cookie,
+        FontAwesomeIcons.cookie,
+        FontAwesomeIcons.candyCane,
+        FontAwesomeIcons.cakeCandles,
+        FontAwesomeIcons.apple,
+      ];
+  static _Candy _buildCandy() => _Candy(
+        size: _random.nextDouble() * 16 + 8,
+        color: Color.lerp(Colors.pink, Colors.purple, _random.nextDouble())!,
+        rotation: _random.nextDouble() * 2 * math.pi,
+        rotationSpeed: _random.nextDouble() * 2 * math.pi,
+        speed: _random.nextDouble() / 2 + .5,
+        direction: _random.nextDouble() * 2 * math.pi,
+        icon: icons[_random.nextInt(icons.length)],
+      );
+  static List<_Candy> _buildCandies() => List<_Candy>.generate(
+        _random.nextInt(maxCandies - minCandies) + minCandies,
+        (index) => _buildCandy(),
+      );
+
+  late final AnimationController _controller = AnimationController(
+    vsync: this,
+    duration: _duration,
+  )..forward();
+  late final Animation<double> _wave1 = CurvedAnimation(parent: _controller, curve: const Interval(0, .5)),
+      _wave2 = CurvedAnimation(parent: _controller, curve: const Interval(.25, .75)),
+      _wave3 = CurvedAnimation(parent: _controller, curve: const Interval(.5, 1)),
+      _percent = TweenSequence<double>(<TweenSequenceItem<double>>[
+        // --- .06 .. .08 --- //
+        TweenSequenceItem<double>(tween: Tween<double>(begin: 0, end: 1), weight: 1),
+        TweenSequenceItem<double>(tween: Tween<double>(begin: 1, end: 1), weight: 8),
+        TweenSequenceItem<double>(tween: Tween<double>(begin: 1, end: 0), weight: 1),
+      ]).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+
+  final List<_Candy> _candies1 = _buildCandies(), _candies2 = _buildCandies(), _candies3 = _buildCandies();
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.addStatusListener(_onStatusChanged);
+    Timer(_duration * .25, () => HapticFeedback.selectionClick().ignore());
+    Timer(_duration * .5, () => HapticFeedback.selectionClick().ignore());
+    Timer(_duration * .75, () => HapticFeedback.selectionClick().ignore());
+    Timer(_duration, () => HapticFeedback.selectionClick().ignore());
+  }
+
+  void _onStatusChanged(AnimationStatus status) {
+    if (status == AnimationStatus.completed) widget.onCompleted();
+  }
+
+  @override
+  void dispose() {
+    _controller
+      ..removeStatusListener(_onStatusChanged)
+      ..dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => Center(
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Center(
+              child: Material(
+                color: Colors.transparent,
+                child: ScaleTransition(
+                  scale: _percent,
+                  child: FadeTransition(
+                    opacity: _percent,
+                    child: ValueListenableBuilder<double>(
+                      valueListenable: _controller,
+                      builder: (context, progress, _) => Text(
+                        '${(progress * 100).round().toString().padLeft(2, '0')}%',
+                        style: GoogleFonts.coiny(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          height: 1,
+                          color: Colors.white,
+                          shadows: <Shadow>[
+                            const BoxShadow(
+                              color: Colors.white24,
+                              offset: Offset(-2, -2),
+                              blurRadius: 8,
+                            ),
+                            const BoxShadow(
+                              color: Colors.black,
+                              offset: Offset(2, 2),
+                              blurRadius: 8,
+                            ),
+                            const BoxShadow(
+                              color: Colors.black,
+                              offset: Offset.zero,
+                              blurRadius: 6,
+                            ),
+                            const BoxShadow(
+                              color: Colors.black,
+                              offset: Offset.zero,
+                              blurRadius: 4,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Positioned.fill(
+              child: Flow(
+                delegate: _CandiesOverlayFlowDelegate(wave: _wave1, candies: _candies1),
+                children: _candies1,
+              ),
+            ),
+            Positioned.fill(
+              child: Flow(
+                delegate: _CandiesOverlayFlowDelegate(wave: _wave2, candies: _candies2),
+                children: _candies2,
+              ),
+            ),
+            Positioned.fill(
+              child: Flow(
+                delegate: _CandiesOverlayFlowDelegate(wave: _wave3, candies: _candies3),
+                children: _candies3,
+              ),
+            ),
+          ],
+        ),
+      );
+}
+
+class _Candy extends StatelessWidget {
+  const _Candy({
+    required this.size,
+    required this.rotation,
+    required this.rotationSpeed,
+    required this.speed,
+    required this.direction,
+    required this.color,
+    required this.icon,
+  });
+
+  final double size, rotation, rotationSpeed, speed, direction;
+  final Color color;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) => Center(
+        child: SizedBox.square(
+          dimension: size,
+          child: Transform.rotate(
+            angle: rotation,
+            child: Icon(
+              icon,
+              color: color,
+              size: size,
+            ),
+          ),
+        ),
+      );
+}
+
+class _CandiesOverlayFlowDelegate extends FlowDelegate {
+  _CandiesOverlayFlowDelegate({required this.wave, required this.candies}) : super(repaint: wave);
+
+  final Animation<double> wave;
+  final List<_Candy> candies;
+
+  @override
+  void paintChildren(FlowPaintingContext context) {
+    if (wave.value == 0 || wave.value == 1) {
+      for (var i = 0; i < context.childCount; i++) {
+        context.paintChild(i, opacity: 0);
+      }
+      return;
+    }
+
+    final size = context.size;
+    final opacity = _getOpacity(wave.value);
+
+    for (var i = 0; i < context.childCount; i++) {
+      final candy = candies[i];
+      final angle = i * 2 * math.pi / context.childCount,
+          radius = (size.shortestSide - candy.size) / 2 * wave.value,
+          x = radius * math.cos(angle) * candy.speed,
+          y = radius * math.sin(angle) * candy.speed;
+      context.paintChild(
+        i,
+        transform: Matrix4.translationValues(x, y, 0),
+        opacity: opacity,
+      );
+    }
+  }
+
+  static double _getOpacity(double progress) {
+    if (progress < .25) return progress * 4;
+    if (progress < .75) return 1;
+    return (1 - progress) * 4;
+  }
+
+  @override
+  bool shouldRepaint(covariant _CandiesOverlayFlowDelegate oldDelegate) => !identical(wave, oldDelegate.wave);
 }
